6f625c2f75ba7e8c0e816c99bcb8c337cf8834b7
Tahara Kazuki <>
2021-12-19 19:31:13 +0900

    update

--- src/server.ts
+++ src/server.ts
@@ -5,7 +5,7 @@ dotenv.config({ path: '../config/config.env' })
 
 const app = express()
 
-const PORT = process.env.PORT || 5000
+const PORT = process.env.PORT || 8000
 
 app.listen(PORT, () => {
   console.info(`Server runnig is ${process.env.NODE_ENV} mode on port ${PORT}`)
